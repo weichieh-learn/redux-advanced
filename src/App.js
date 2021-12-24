3f7fd0440fd87233b51a3516e37d882cb14c35a2
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-12-24 16:14:05 +0800

    使用useEffect送http req

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
 import Cart from './components/Cart/Cart'
@@ -5,15 +6,24 @@ import Layout from './components/Layout/Layout'
 import Products from './components/Shop/Products'
 
 function App() {
-
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible)
+  const cart = useSelector((state) => state.cart)
+
+  useEffect(() => {
+    // 當cart改變的時候，會觸發useEffect去送http req把cart更新到firebase
+    // PUT: overwrtite existing cart
+    fetch(
+      'https://redux-advanced-476ea-default-rtdb.firebaseio.com/cart.json',
+      { method: 'PUT', body: JSON.stringify(cart) }
+    )
+  }, [cart])
 
   return (
     <Layout>
       {cartIsVisible && <Cart />}
       <Products />
     </Layout>
-  );
+  )
 }
 
-export default App;
+export default App
