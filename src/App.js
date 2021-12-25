4f32f701b4bea7e1c6dfdd6227357576e309ce1b
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2021-12-25 20:47:49 +0800

    使用Action Creator Thunk 來寫async function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
-import { uiActions } from './store/ui-slice'
-
 import Cart from './components/Cart/Cart'
 import Layout from './components/Layout/Layout'
 import Products from './components/Shop/Products'
 import Notification from './components/UI/Notification'
+import { sendCartData } from './store/cart-slice'
 
 // 定義在App元件之外，不會被改變，也不會在component被重新render的時候被重置
 // 只會在file被第一次打開的時候被重置
@@ -19,48 +18,13 @@ function App() {
   const notification = useSelector((state) => state.ui.notification)
 
   useEffect(() => {
-    // 當cart改變的時候，會觸發useEffect去送http req把cart更新到firebase
-    // PUT: overwrtite existing cart
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      )
-      const res = await fetch(
-        'https://redux-advanced-476ea-default-rtdb.firebaseio.com/cart.json',
-        { method: 'PUT', body: JSON.stringify(cart) }
-      )
-      if(!res.ok) {
-        throw new Error('Sending cart data failed.')
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sending data successfully!',
-        })
-      )
-    }
-
     if(isInitial) {
       // 如果網頁第一次打開，不要執行sendCartData()
       isInitial = false
       return
     }
 
-    sendCartData().catch(error => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending data failed!',
-        })
-      )
-    })
+    dispatch(sendCartData(cart))
   }, [cart, dispatch])
 
   return (
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,44 +1,95 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from '@reduxjs/toolkit'
+
+import { uiActions } from './ui-slice'
 
 const cartSlice = createSlice({
-    name: 'cart',
-    initialState: {
-        items: [],
-        totalQty: 0,
-        // cartPrice: 0
+  name: 'cart',
+  initialState: {
+    items: [],
+    totalQty: 0,
+    // cartPrice: 0
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload
+      const existingItem = state.items.find((item) => item.id === newItem.id) //是否已經有加入此商品
+      state.totalQty++
+      if (!existingItem) {
+        // must NOT do in Redux, but Redux-toolkit is fine
+        state.items.push({
+          id: newItem.id,
+          title: newItem.title,
+          price: newItem.price,
+          qty: 1,
+          totalPrice: newItem.price,
+        })
+      } else {
+        existingItem.qty++
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price
+      }
     },
-    reducers: {
-        addItemToCart(state, action) {
-            const newItem = action.payload
-            const existingItem = state.items.find(item => item.id === newItem.id) //是否已經有加入此商品
-            state.totalQty++
-            if(!existingItem) {
-                // must NOT do in Redux, but Redux-toolkit is fine 
-                state.items.push({
-                    id: newItem.id,
-                    title: newItem.title,
-                    price: newItem.price,
-                    qty: 1,
-                    totalPrice: newItem.price,
-                })
-            } else {
-                existingItem.qty++
-                existingItem.totalPrice = existingItem.totalPrice + newItem.price
-            }
-        },
-        removeItemFromCart(state, action) {
-            const id = action.payload
-            const existingItem = state.items.find(item => item.id === id)
-            state.totalQty--
-            if (existingItem.qty === 1) {
-                state.items = state.items.filter(item => item.id !== id)
-            } else {
-                existingItem.qty--
-                existingItem.totalPrice = existingItem.totalPrice - existingItem.price
-            }
-        }
-    }
+    removeItemFromCart(state, action) {
+      const id = action.payload
+      const existingItem = state.items.find((item) => item.id === id)
+      state.totalQty--
+      if (existingItem.qty === 1) {
+        state.items = state.items.filter((item) => item.id !== id)
+      } else {
+        existingItem.qty--
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price
+      }
+    },
+  },
 })
+
+export const sendCartData = (cart) => {
+  // return a async function
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data!',
+      })
+    )
+
+    // create another async function(send Http req)
+    const sendRequest = async () => {
+      const res = await fetch(
+        'https://redux-advanced-476ea-default-rtdb.firebaseio.com/cart.json',
+        {
+          method: 'PUT', // PUT: overwrtite existing cart
+          body: JSON.stringify(cart),
+        }
+      )
+      if (!res.ok) {
+        throw new Error('Sending cart data failed.')
+      }
+    }
+
+    try {
+      //execute (send Http req)
+      await sendRequest()
+      //if no error
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success!',
+          message: 'Sending data successfully!',
+        })
+      )
+    } catch (error) {
+      //if error
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Sending data failed!',
+        })
+      )
+    }
+  }
+}
 
 export const cartActions = cartSlice.actions
 
